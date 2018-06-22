3808113616d9e94068a45e72fe74b68efca78b10
Stephen Poole <stephenwpoole@gmail.com>
2018-06-22 17:30:01 -0400

    Added git repo. Removed unneeded enums.

--- app/js/enum/index.js
+++ app/js/enum/index.js
@@ -1,19 +1,3 @@
-export const COMPONENT_REFS = {
-    HEADER_CONTROLLER: 'header',
-    FEATURES_CONTROLLER: 'features',
-    CONTEST_CONTROLLER: 'contest',
-    TWITTER_CONTROLLER: 'twitter',
-    GALLERY_CONTROLLER: 'gallery'
-};
-
-export const FEATURES = {
-    VORMAX_FLUSHING: 0,
-    VORMAX_PLUS_TOILET: 1,
-    ADVANCED_CLEAN_BIDET: 2,
-    SPECTRA: 3,
-    MEASUREFILL: 4
-};
-
 export const LANGUAGES = {
     EN: 'en',
     FR: 'fr'
